b3141a9f19adf2709f5068e4d76d071dd093eb77
jeff ngugi <ngugimwathi254@gmail.com>
2025-11-19 12:39:27 +0000

    Update API URLs to use CODESPACE_NAME and allow codespace/localhost hosts

--- octofit-tracker/backend/octofit_tracker/models.py
+++ octofit-tracker/backend/octofit_tracker/models.py
@@ -1,37 +1,5 @@
-from djongo import modelsfrom djongo import models
+from django.db import models
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        app_label = 'octofit_tracker'    class Meta:    difficulty = models.CharField(max_length=50)    name = models.CharField(max_length=100)class Workout(models.Model):        app_label = 'octofit_tracker'    class Meta:    points = models.IntegerField()    team = models.CharField(max_length=50)class Leaderboard(models.Model):        app_label = 'octofit_tracker'    class Meta:    duration = models.IntegerField()    type = models.CharField(max_length=50)    user = models.CharField(max_length=100)class Activity(models.Model):        app_label = 'octofit_tracker'    class Meta:    name = models.CharField(max_length=50, unique=True)class Team(models.Model):        app_label = 'octofit_tracker'    class Meta:    team = models.CharField(max_length=50)    name = models.CharField(max_length=100)    email = models.EmailField(unique=True)class User(models.Model):
 class User(models.Model):
     email = models.EmailField(unique=True)
     name = models.CharField(max_length=100)
